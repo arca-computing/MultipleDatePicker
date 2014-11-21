639a51a71165bc2861b9177228e0f33386affcfd
Antoine Bluchet <abluchet@ds-restauration.com>
2014-11-21 15:30:06 +0100

    docs(): Document day-click and day-hover

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,18 @@
 var app = angular.module('demoApp', ['multipleDatePicker']);
 
 app.controller('demoController', ['$scope', function($scope){
-	$scope.logInfos = function(timestamp, selected){
-		alert(moment(timestamp).format('YYYY-M-DD') + ' has been ' + (selected ? '' : 'un') + 'selected');
-	};
+  $scope.logInfos = function(time, selected) {
+    alert(moment(time).format('YYYY-M-DD') + ' has been ' + (selected ? '' : 'un') + 'selected');
+  }
 
-	$scope.oneDayOff = [moment().date(14).valueOf()];
-	$scope.selectedDays = [moment().date(4).valueOf(), moment().date(5).valueOf(), moment().date(8).valueOf()];
+  $scope.doDate = function(event, date){
+    if(event.type == 'click') {
+      alert(moment(date).format('YYYY-M-DD') + ' has been ' + (date.selected ? 'un' : '') + 'selected');
+    } else {
+      console.log(moment(date) + ' has been ' + event.type + 'ed')
+    }
+  };
+
+  $scope.oneDayOff = [moment().date(14).valueOf()];
+  $scope.selectedDays = [moment().date(4).valueOf(), moment().date(5).valueOf(), moment().date(8).valueOf()];
 }]);
