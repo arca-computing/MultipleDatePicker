f8cacacd43b22235797d4d9bfa0de631904ee769
Matthew Sabol <sabol_matthew@bah.com>
2015-07-22 20:01:51 -0400

    Updated demo

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,22 +1,27 @@
 var app = angular.module('demoApp', ['multipleDatePicker']);
 
-app.controller('demoController', ['$scope', function($scope){
-  $scope.logInfos = function(time, selected) {
-    alert(moment(time).format('YYYY-M-DD') + ' has been ' + (selected ? '' : 'un') + 'selected');
-  };
+app.controller('demoController', ['$scope', function ($scope) {
+    $scope.logInfos = function (time, selected) {
+        alert(moment(time).format('YYYY-M-DD') + ' has been ' + (selected ? '' : 'un') + 'selected');
+    };
 
-  $scope.logMonthChanged = function(newMonth, oldMonth){
-    alert('new month : ' + newMonth.format('YYYY-M-DD') + ' || old month : ' + oldMonth.format('YYYY-M-DD'));
-  };
+    $scope.logMonthChanged = function (newMonth, oldMonth) {
+        alert('new month : ' + newMonth.format('YYYY-M-DD') + ' || old month : ' + oldMonth.format('YYYY-M-DD'));
+    };
 
-  $scope.doDate = function(event, date){
-    if(event.type == 'click') {
-      alert(moment(date).format('YYYY-M-DD') + ' has been ' + (date.selected ? 'un' : '') + 'selected');
-    } else {
-      console.log(moment(date) + ' has been ' + event.type + 'ed')
-    }
-  };
+    $scope.doDate = function (event, date) {
+        if (event.type == 'click') {
+            alert(moment(date).format('YYYY-M-DD') + ' has been ' + (date.selected ? 'un' : '') + 'selected');
+        } else {
+            console.log(moment(date) + ' has been ' + event.type + 'ed')
+        }
+    };
 
-  $scope.oneDayOff = [moment().date(14).valueOf()];
-  $scope.selectedDays = [moment().date(4).valueOf(), moment().date(5).valueOf(), moment().date(8).valueOf()];
+    $scope.oneDayOff = [moment().date(14).valueOf()];
+    $scope.highlightDays = [
+        {date: moment().date(2).valueOf(), css: 'holiday', selectable: false,title: 'Holiday time !'},
+        {date: moment().date(14).valueOf(), css: 'off', selectable: false,title: 'We don\'t work today'},
+        {date: moment().date(25).valueOf(), css: 'birthday', selectable: true,title: 'I\'m thir... i\'m 28, seriously, I mean ...'}
+    ];
+    $scope.selectedDays = [moment().date(4).valueOf(), moment().date(5).valueOf(), moment().date(8).valueOf()];
 }]);
