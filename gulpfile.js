d2379d68ac32f2003ace95118f22a91b8c2792ed
Chris Olsen <chris@chrisolsen.net>
2016-08-17 08:41:36 -0400

    Added lint-app gulp task back in

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('lint-app', function () {
         .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('uglify', function () {
+gulp.task('uglify',['lint-app'], function () {
     return gulp.src('./multipleDatePicker.js')
         .pipe(uglify())
         .pipe(rename('multipleDatePicker.min.js'))
