59bdca12304701d08622af63d84fbaa099b444bf
Maelig GOHIN <mgn@ehtrace.com>
2017-08-16 08:50:52 +0200

    release: 2.1.4

--- multipleDatePicker.js
+++ multipleDatePicker.js
@@ -1,6 +1,6 @@
 /*
  @author : Maelig GOHIN For ARCA-Computing - www.arca-computing.fr
- @version: 2.1.3.1
+ @version: 2.1.4
 
  @description:  MultipleDatePicker is an Angular directive to show a simple calendar allowing user to select multiple dates.
  Css style can be changed by editing less or css stylesheet.
